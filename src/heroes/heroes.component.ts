23c90e1fd5f69bc39b4a86b1330c7013b5a88fc9
kvandana451 <kvandana451@gmail.com>
2024-12-27 18:12:33 +0530

    displays list of heroes using ngFor,The user can select a hero and see that hero's details,used class binding for selected heroo

--- src/heroes/heroes.component.ts
+++ src/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { heroes } from '../data/heroes';
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-heroes',
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    name: 'Windstorm',
-    id: 1,
-  };
+  // hero: Hero = {
+  //   name: 'Windstorm',
+  //   id: 1,
+  // };
+  heroes: Hero[] = heroes;
+  isSelected?: Hero;
+  onSelect(data: Hero) {
+    this.isSelected = data;
+  }
+  ngOnInit() {
+    // console.log(heroes);
+    console.log(this.isSelected);
+  }
 }
